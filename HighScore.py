145b259b3ef12ebf4e36b197debfa7325e1b9074
Ted Henriksson <ted@henrikssons.net>
2016-03-25 16:11:03 +0100

    Highscore.py converted to py3 and pep8.

--- HighScore.py
+++ HighScore.py
@@ -2,40 +2,30 @@
 # -*- coding: utf-8 -*-
 
 import sqlite3 as lite
-import sys
-import time
 import os
 import time
 
-while 1==1:
+while True:
     os.system('clear')
     con = lite.connect('People.db')
     with con:
         cur = con.cursor()
-      #  cur.execute("CREATE TABLE People(Id INT, blipId INT, Nick TEXT, isHere INT, totalTime FLOAT, lastLogin FLOAT)")
 
-        print "------------ Highscore -----------"
+        print("------------ Highscore -----------")
 
-        #cur.execute("SELECT * FROM People WHERE blipId = ?", (rfId,))
         cur.execute("SELECT * FROM People ORDER BY totalTime DESC")
         rows = cur.fetchall()
-        i=1
-        for row in rows:
-            print str(i) + ": " + str(row[2]).ljust(10) + " score: " + str(row [4])
-            i = i+1;
 
-        print "------------- People online ------"
+        for i in range(0, len(rows)):
+            print(str(i+1) + ": " + str(rows[i][2]).ljust(10) +
+                  " score: " + str(rows[i][4]))
+
+        print("------------- People online ------")
 
         cur.execute("SELECT * FROM People WHERE isHere = 1")
         rows = cur.fetchall()
-        i=1
+
         for row in rows:
-            print str(row[2]).ljust(10) 
-            i = i+1;       
+            print(str(row[2]).ljust(10))
+
     time.sleep(15)
-
-
-
-
-
-
