75c7ad89440d2cde6bb68a6ef898ebb5a4bbcda9
Ted Henriksson <ted@henrikssons.net>
2017-04-10 20:34:55 +0200

    Simple fix so that we find the db "dynamically" when executing from cron.

--- logout_all.py
+++ logout_all.py
@@ -1,5 +1,10 @@
 import sqlite3 as lite
-con = lite.connect('People.db')
+import os
+
+path, _ = os.path.split(os.path.realpath(__file__))
+db_path = os.path.join(path, 'People.db')
+
+con = lite.connect(db_path)
 with con:
     cur = con.cursor()
     cur.execute("UPDATE People SET isHere = 0")
