476e79f79d2a9bbad38d003d29672fdf90b86fee
Ted Henriksson <ted@henrikssons.net>
2017-04-11 13:00:39 +0200

    Uppdated model to have more pythonific variable names

--- models.py
+++ models.py
@@ -2,12 +2,13 @@ from peewee import *
 
 db = SqliteDatabase('People.db', **{})
 
+
 class Person(Model):
     nick = TextField(db_column='Nick', null=False)
-    blipId = IntegerField(db_column='blipId', null=False)
-    ishere = BooleanField(db_column='isHere', null=False, default=False)
-    lastlogin = FloatField(db_column='lastLogin', null=False)  # FLOAT
-    totaltime = FloatField(db_column='totalTime', null=True)  # FLOAT
+    blip_id = IntegerField(db_column='blipId', null=False)
+    is_here = BooleanField(db_column='isHere', null=False, default=False)
+    last_login = FloatField(db_column='lastLogin', null=False, default=0.0)  # FLOAT
+    total_time = FloatField(db_column='totalTime', null=True, default=0.0)  # FLOAT
 
     class Meta:
         database = db
