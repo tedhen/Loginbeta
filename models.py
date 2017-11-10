9dc0d595c64158f4bd253f5bebe7c51e73c9c712
Ted Henriksson <ted@henrikssons.net>
2017-11-10 09:59:24 +0100

    New db models.

--- models.py
+++ models.py
@@ -5,7 +5,7 @@ db = SqliteDatabase('People.db', **{})
 
 class Person(Model):
     nick = TextField(db_column='Nick', null=False)
-    blip_id = IntegerField(db_column='blipId', null=False)
+    blip_id = FixedCharField(db_column='blipId', null=False, max_length=128)
     is_here = BooleanField(db_column='isHere', null=False, default=False)
     last_login = FloatField(db_column='lastLogin', null=False, default=0.0)  # FLOAT
     total_time = FloatField(db_column='totalTime', null=True, default=0.0)  # FLOAT
@@ -23,3 +23,16 @@ class DbVersion(Model):
     class Meta:
         database = db
         db_table = 'DbVersion'
+
+
+class LegacyPerson(Model):
+    Id = IntegerField(db_column='Id', null=False)
+    nick = TextField(db_column='Nick', null=False)
+    blip_id = IntegerField(db_column='blipId', null=False)
+    is_here = BooleanField(db_column='isHere', null=False, default=False)
+    last_login = FloatField(db_column='lastLogin', null=False, default=0.0)  # FLOAT
+    total_time = FloatField(db_column='totalTime', null=True, default=0.0)  # FLOAT
+
+    class Meta:
+        database = db
+        db_table = 'People_legacy'
